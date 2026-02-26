9dc7b4ed280cd694429618a7cfe729891f682310
Muslima Ismoiljonova <muslimaismoiljonova272@gmail.com>
2026-02-26 13:26:02 +0500

    JavaScript learning

---
+++ vazifa/script.js
@@ -0,0 +1,35 @@
+// 1-vazifa
+let firstName = "Muslima";
+let lastName = "Ismoiljonova";
+let birthYear = 2007;
+let city = "Tashkent";
+const year = 2026;
+
+console.log(`Salom, mening ismim:`, firstName);
+console.log(`Men`, birthYear, "yilda tug'ilganman");
+console.log(
+  `Hozirda ${year - birthYear} yoshdaman va ${city} shahrida yashayman`,
+);
+
+// 3-vazifa
+let ageInput = "25";
+let scoreInput = "100";
+
+console.log(Number(ageInput));
+console.log(Number(scoreInput));
+
+console.log(Number(ageInput + scoreInput));
+
+let unknownValue = "JavaScript";
+console.log(typeof unknownValue);
+
+// 4-vazifa
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(""));
+console.log(Boolean(" "));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(NaN));
